c187e60c51ef263a4772761735c451f6b0b577ed
p-laranjinha <plcasimiro@ua.pt>
2022-04-25 01:51:42 +0100

    Limited max acceleration decimal place count to prevent overflow

--- P1/02/common.py
+++ P1/02/common.py
@@ -2,4 +2,7 @@ def velocity(previousPos, currentPos, timeInterval):
     return (currentPos - previousPos) / timeInterval
 
 def acceleration(velocity):
-    return -acceleration.g + acceleration.u / acceleration.m * velocity**2
+    acc = -acceleration.g + acceleration.u / acceleration.m * velocity**2
+    if acc < 1e-100: # stops acceleration overflow (too many decimal places) if it becomes too small
+        return 0
+    return acc
