0dd96320303782217f1fef8a1a79ddd65c2b5d2f
p-laranjinha <plcasimiro@ua.pt>
2022-04-25 02:59:16 +0100

    fixed last commit, just catches overflow instead

--- P1/02/common.py
+++ P1/02/common.py
@@ -2,7 +2,8 @@ def velocity(previousPos, currentPos, timeInterval):
     return (currentPos - previousPos) / timeInterval
 
 def acceleration(velocity):
-    acc = -acceleration.g + acceleration.u / acceleration.m * velocity**2
-    if acc < 1e-100: # stops acceleration overflow (too many decimal places) if it becomes too small
-        return 0
-    return acc
+    try:
+        return -acceleration.g + acceleration.u / acceleration.m * velocity**2
+    except:
+        print("Wind resistance value caused an overflow by making acceleration too big.")
+        exit(1);
