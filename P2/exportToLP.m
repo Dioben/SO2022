6ff193ac7ed67188f6aa982ac6058d4da2fb048a
buckaroo69 <diogobento@ua.pt>
2022-06-10 18:09:28 +0100

    LPT work done

---
+++ P2/exportToLP.m
@@ -0,0 +1,43 @@
+function []= exportToLP(nodeCount,linkMatrix,critical,filename)
+
+fid = fopen(filename,'wt');
+fprintf(fid,'min ');
+for i=1:nodeCount
+    for j=i+1:nodeCount
+        fprintf(fid,'+ l%d_%d',i,j);
+    end
+end
+
+
+fprintf(fid,'\nsubject to\n\\\\only %d can be critical\n',critical);
+for i=1:nodeCount %%enforce critical node count condition
+    fprintf(fid,'+ n%d',i);
+end
+fprintf(fid,' = %d\n',critical);
+
+for i= 1:nodeCount-1 
+    for j= i+1:nodeCount
+        if linkMatrix(i,j)>0 %%map out links we know exist
+            fprintf(fid,'\\\\this is a known pair\n+ l%d_%d + n%d + n%d >=1\n',i,j,i,j);
+        else %%scope out neighbourhood for possible connections
+            fprintf(fid,'\\\\no direct link between %d,%d\n',i,j);
+            for k=find(linkMatrix(i,:)>0)
+                fprintf(fid,'+ l%d_%d - l%d_%d - l%d_%d - n%d>= -1\n', i,j, i,k, k,j ,k );
+            end
+        end
+    end
+end
+
+
+
+fprintf(fid,'binary\n');
+for i=1:nodeCount
+    fprintf(fid,'n%d ',i);
+end
+for i=1:nodeCount
+    for j=i+1:nodeCount
+        fprintf(fid,'l%d_%d ',i,j);
+    end
+end
+fprintf(fid,'\nend');
+fclose(fid);
