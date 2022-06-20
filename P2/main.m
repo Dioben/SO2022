c19cf6dd4d2e4e2bc321240e0381edb45b8503e8
p-laranjinha <plcasimiro@ua.pt>
2022-06-21 00:09:53 +0100

    results

--- P2/main.m
+++ P2/main.m
@@ -5,29 +5,28 @@ G = graph(L);
 % plotTopology(Nodes,Links,[]);
 
 % exportToLP(size(Nodes,1),L,8,"critical8.lpt");
+% exportToLP(size(Nodes,1),L,12,"critical12.lpt");
 
-results = [];
-for n=[8,12]
-for time=[5,10,20]
-for r=[1,2,3,4,5,10]
-results = [results; n time r GRASP(G,n,time,r,@ConnectedNP,@SAHillClimb1) GRASP(G,n,time,r,@ConnectedNP,@SAHillClimb2)];
-writematrix(results,"GRASP.csv");
-end
-end
-end
+% results = [];
+% for n=[8,12]
+% for time=[60]
+% for r=[1,2,3,4,5,6,7,8,9,10]
+% results = [results; n time r GRASP(G,n,time,r,@ConnectedNP,@SAHillClimb1) GRASP(G,n,time,r,@ConnectedNP,@SAHillClimb2)];
+% writematrix(results,"GRASP.csv");
+% end
+% end
+% end
 
-results = [];
-for n=[8,12]
-for time=[5,10,20]
-for nPop=[5,10,20,30]
-for elitism=[1,2,3,4]
-for mutChance=[0,0.25,0.5,0.75,1]
-if nPop>elitism
-results = [results; n time nPop elitism mutChance Memetic(G,n,time,nPop,elitism,mutChance,@ConnectedNP,@SAHillClimb1) Memetic(G,n,time,nPop,elitism,mutChance,@ConnectedNP,@SAHillClimb2)];
-writematrix(results,"Memetic.csv");
-end
-end
-end
-end
-end
-end
+% results = [];
+% for n=[8,12]
+% for time=[30]
+% for nPop=[5,10,15]
+% for elitism=[1,2,3,4]
+% for mutChance=[0,0.25,0.5,0.75,1]
+% results = [results; n time nPop elitism mutChance Memetic(G,n,time,nPop,elitism,mutChance,@ConnectedNP,@SAHillClimb1) Memetic(G,n,time,nPop,elitism,mutChance,@ConnectedNP,@SAHillClimb2)];
+% writematrix(results,"Memetic.csv");
+% end
+% end
+% end
+% end
+% end
