8a67af2ea33322fe1117c6fd57d5409113d2502c
p-laranjinha <plcasimiro@ua.pt>
2022-06-20 16:49:25 +0100

    main now tests args in results
    tests should take ~3h30m

--- P2/main.m
+++ P2/main.m
@@ -4,7 +4,30 @@ G = graph(L);
 % figure(1);
 % plotTopology(Nodes,Links,[]);
 
-[graspRes,graspNodes]= GRASP(G,8,30,4,@ConnectedNP,@SAHillClimb1)
-[memRes,memNodes]= Memetic(G,8,30,10,1,0.1,@ConnectedNP,@SAHillClimb2)
 % exportToLP(size(Nodes,1),L,8,"critical8.lpt");
-% greedyRandom(G,8,1,@ConnectedNP)
+
+results = [];
+for n=[8,12]
+for time=[5,10,20]
+for r=[1,2,3,4,5,10]
+results = [results; n time r GRASP(G,n,time,r,@ConnectedNP,@SAHillClimb1) GRASP(G,n,time,r,@ConnectedNP,@SAHillClimb2)];
+writematrix(results,"GRASP.csv");
+end
+end
+end
+
+results = [];
+for n=[8,12]
+for time=[5,10,20]
+for nPop=[5,10,20,30]
+for elitism=[1,2,3,4]
+for mutChance=[0,0.25,0.5,0.75,1]
+if nPop>elitism
+results = [results; n time nPop elitism mutChance Memetic(G,n,time,nPop,elitism,mutChance,@ConnectedNP,@SAHillClimb1) Memetic(G,n,time,nPop,elitism,mutChance,@ConnectedNP,@SAHillClimb2)];
+writematrix(results,"Memetic.csv");
+end
+end
+end
+end
+end
+end
