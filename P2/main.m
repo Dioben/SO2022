063085b9ac7ebd8470327a7667cfdbca1b476831
p-laranjinha <plcasimiro@ua.pt>
2022-06-21 21:51:08 +0100

    analysis

--- P2/main.m
+++ P2/main.m
@@ -1,33 +1,73 @@
 [Nodes,Links,L]= generateTopology(93391);
 G = graph(L);
 
-% figure(1);
-% plotTopology(Nodes,Links,[]);
+%% Show node graphs
+if false
+    figure(1);
+    plotTopology(Nodes,Links,[]);
+    figure(2);
+    plotTopology(Nodes,Links,[16,26,30,33,40,66,73,92]);
+    ConnectedNP(G,[16,26,30,33,40,66,73,92])
+    figure(3);
+    plotTopology(Nodes,Links,[16,26,28,33,40,48,57,61,69,73,86,97]);
+    ConnectedNP(G,[16,26,28,33,40,48,57,61,69,73,86,97])
+end
 
-% exportToLP(size(Nodes,1),L,8,"critical8.lpt");
-% exportToLP(size(Nodes,1),L,12,"critical12.lpt");
+%% Generate lpt
+if false
+    exportToLP(size(Nodes,1),L,8,"critical8.lpt");
+    exportToLP(size(Nodes,1),L,12,"critical12.lpt");
+end
 
-% results = [];
-% for n=[8,12]
-% for time=[1,30,60]
-% for r=[1,2,3,4,5,6,7,8,9,10]
-% results = [results; n time r GRASP(G,n,time,r,@ConnectedNP,@SAHillClimb1)];
-% writematrix(results,"GRASP.csv");
-% end
-% end
-% end
+%% Grasp arg testing
+if false
+    results = [];
+    for n=[8,12]
+    for time=[1,30,60]
+    for r=[1,2,3,4,5,6,7,8,9,10]
+    results = [results; n time r GRASP(G,n,time,r,@ConnectedNP,@SAHillClimb1)];
+    writematrix(results,"GRASP.csv");
+    end
+    end
+    end
+end
 
-% results = [];
-% for n=[8,12]
-% for time=[1,30]
-% for nPop=[5,10,15]
-% for elitism=[1,2,3,4]
-% for mutChance=[0,0.1,0.25,0.5,0.75,1]
-% results = [results; n time nPop elitism mutChance Memetic(G,n,time,nPop,elitism,mutChance,@ConnectedNP,@SAHillClimb1) Memetic(G,n,time,nPop,elitism,mutChance,@ConnectedNP,@SAHillClimb2)];
-% writematrix(results,"Memetic.csv");
-% end
-% end
-% end
-% end
-% end
+%% Memetic arg testing
+if false
+    results = [];
+    for n=[8,12]
+    for time=[1,30]
+    for nPop=[5,10,15]
+    for elitism=[1,2,3,4]
+    for mutChance=[0,0.1,0.25,0.5,0.75,1]
+    results = [results; n time nPop elitism mutChance Memetic(G,n,time,nPop,elitism,mutChance,@ConnectedNP,@SAHillClimb1) Memetic(G,n,time,nPop,elitism,mutChance,@ConnectedNP,@SAHillClimb2)];
+    writematrix(results,"Memetic.csv");
+    end
+    end
+    end
+    end
+    end
+end
+
+%% GRASP final results
+if false
+    results = [];
+    for i=1:10
+        results = [results; 8 GRASP(G,8,300,4,@ConnectedNP,@SAHillClimb1)];
+        results = [results; 12 GRASP(G,12,300,8,@ConnectedNP,@SAHillClimb1)];
+        writematrix(results,"GRASPFinal.csv");
+    end
+end
+
+%% Memetic final results
+if false
+    results = [];
+    for i=1:10
+        t = tic();
+        results = [results; 8 Memetic(G,8,300,15,2,0.75,@ConnectedNP,@SAHillClimb1) toc(t)];
+        t = tic();
+        results = [results; 12 Memetic(G,12,300,15,2,0.25,@ConnectedNP,@SAHillClimb1) toc(t)];
+        writematrix(results,"MemeticFinal.csv");
+    end
+end
 
--- P2/plot.py
+++ P2/plot.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.express as px
 
-### GRASP
+### GRASP tests
 if False:
     df = pd.read_csv("GRASP.csv", names=["n","time","r","LS1","LS2"])
 
@@ -10,7 +10,7 @@ if False:
     fig = px.line(df, x="r", y="LS1", color="n", title="Average results per r")
     fig.show()
 
-### Memetic
+### Memetic tests
 if False:
     df = pd.read_csv("Memetic.csv", names=["n","time","population size","elitism","mutation chance","LS1","LS2"])
 
@@ -18,4 +18,24 @@ if False:
 
     df = df.groupby(["n",col]).mean().reset_index()
     fig = px.line(df, x=col, y="LS1", color="n", title=col)
+    fig.show()
+
+### GRASP final
+if False:
+    df = pd.read_csv("GRASP5min.csv", names=["n","result"])
+    df2 = df.groupby(["n"]).agg({"result":["mean","min","max"]})
+    print(df2)
+
+    df["run"] = df.groupby(["n"]).cumcount()+1
+    fig = px.line(df, x="run", y="result", color="n", title="5 min GRASP")
+    fig.show()
+
+### Memetic final
+if False:
+    df = pd.read_csv("MemeticBest.csv", names=["n","result","time"])
+    df2 = df.groupby(["n"]).agg({"time":["mean","min","max"]})
+    print(df2)
+
+    df["run"] = df.groupby(["n"]).cumcount()+1
+    fig = px.line(df, x="run", y="time", color="n", title="Best memetic")
     fig.show()
